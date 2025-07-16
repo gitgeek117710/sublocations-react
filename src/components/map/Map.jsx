abde3902f8fba5195c5f5c4fa108e4367e19d389
gitgeek117710 <gitgeek117710@gmail.com>
2025-07-16 11:40:32 +0800

    Add new files and features

--- src/components/map/Map.jsx
+++ src/components/map/Map.jsx
@@ -1,11 +1,55 @@
 import React from 'react'
+import { MapContainer, TileLayer, GeoJSON } from 'react-leaflet'
+import 'leaflet/dist/leaflet.css';
+import { useRef } from 'react';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import '../map/map.css';
 
 export const Map = () => {
-  return (
-    <>
-      <div>
-        <h4>Sublocations</h4>
-        </div>  
-    </>
-  )
+    const mapRef = useRef();
+    const position = [-1.2928, 36.8219];
+    const [geodata, setGeoData] = useState(null);
+    useEffect(() => {
+        fetch('/data/nairobi_sublocs.geojson')
+            .then((res) => res.json())
+            .then((data) => setGeoData(data))
+            .catch((err) => console.error('Failed to load GeoJSON', err))
+    });
+
+    // styles
+    const geoStyle = {
+        fillColor: "#3388ff",
+        fillOpacity: 0,
+        color: "yellow",
+        weight: 3
+    }
+
+    const onEachFeature = (feature, layer) => {
+        const name = feature.properties?.SLNAME || "Unnamed";
+        layer.bindTooltip(name, {
+            permanent: true,
+            direction: "center",
+            className: "geo-label"
+        });
+    }
+
+    return (
+        <div>
+            <h4>Sublocations</h4>
+            <MapContainer
+                center={position}
+                zoom={13}
+                scrollWheelZoom={true}
+                style={{ height: '400px', width: '800px' }} // 🔥 Required
+                ref={mapRef}
+            >
+                <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                {geodata && <GeoJSON data={geodata} style={geoStyle} onEachFeature={onEachFeature} />}
+            </MapContainer>
+        </div>
+    )
 }
